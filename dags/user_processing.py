1fcb1d4843e4e8763ef3beed0107db250aab6735
Avin Varghese Mathew <avinvmavolil@gmail.com>
2025-12-29 22:24:16 +0530

    included task decorator instead of python operator

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,32 +1,6 @@
 from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
-from airflow.providers.standard.operators.python import PythonOperator
-
-def _extract_user(ti):
-    fake_user = ti.xcom_pull(task_ids = "is_api_available")
-    return {
-        "id" : fake_user["id"],
-        "firstname" : fake_user["personalInfo"]["firstName"],
-        "lastname" :fake_user["personalInfo"]["lastName"],
-        "email" : fake_user["personalInfo"]["email"]
-    }
-
-# # testing _extract_user
-# def _extract_user(ti):
-#     import requests
-#     response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/fcad9e032e8d0c54f8dd40830efd7858d73e92d2/fakeuser.json")
-#     # print(response.status_code)
-#     fake_user = response.json()
-#     # print(fake_user)
-#     return {
-#         "id" : fake_user["id"],
-#         "firstname" : fake_user["personalInfo"]["firstName"],
-#         "lastname" :fake_user["personalInfo"]["lastName"],
-#         "email" : fake_user["personalInfo"]["email"]
-#     }
-    
-# # _extract_user()
 
 @dag
 def user_processing():
@@ -58,17 +32,73 @@ def user_processing():
         
         return PokeReturnValue(is_done = condition, xcom_value = fake_user)
     
-    extract_user = PythonOperator(
-        task_id = "extract_user",
-        python_callable = _extract_user
-    )
+    # using task - task decorator is python operator
+    @task
+    def extract_user(fake_user):
+        return {
+            "id" : fake_user["id"],
+            "firstname" : fake_user["personalInfo"]["firstName"],
+            "lastname" :fake_user["personalInfo"]["lastName"],
+            "email" : fake_user["personalInfo"]["email"]
+        }
     
-    is_api_available()
+    fake_user = is_api_available()
+    user_info = extract_user(fake_user)
 
 user_processing()
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #using python operator
+# from airflow.providers.standard.operators.python import PythonOperator
+
+# def _extract_user(ti):
+#     fake_user = ti.xcom_pull(task_ids = "is_api_available")
+#     return {
+#         "id" : fake_user["id"],
+#         "firstname" : fake_user["personalInfo"]["firstName"],
+#         "lastname" :fake_user["personalInfo"]["lastName"],
+#         "email" : fake_user["personalInfo"]["email"]
+#     }
+
+# # # testing _extract_user
+# # def _extract_user(ti):
+# #     import requests
+# #     response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/fcad9e032e8d0c54f8dd40830efd7858d73e92d2/fakeuser.json")
+# #     # print(response.status_code)
+# #     fake_user = response.json()
+# #     # print(fake_user)
+# #     return {
+# #         "id" : fake_user["id"],
+# #         "firstname" : fake_user["personalInfo"]["firstName"],
+# #         "lastname" :fake_user["personalInfo"]["lastName"],
+# #         "email" : fake_user["personalInfo"]["email"]
+# #     }
+    
+# # # _extract_user()
+
+
+# extract_user = PythonOperator(
+#         task_id = "extract_user",
+#         python_callable = _extract_user
+#     )
 
 
